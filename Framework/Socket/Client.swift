29d6ee2536ab95d25537231cdef41a1a3de19db3
Sofia Diniz Melo Santos <sdms@cin.ufpe.br>
2025-03-25 17:31:41 +0100

    Cleaned up the connection statuses. Pushing this to my branch to execute tests

--- Framework/Socket/Client.swift
+++ Framework/Socket/Client.swift
@@ -9,14 +9,30 @@ import Foundation
 import Network
 import SwiftUI
 
-public final class FinalClient {
+public final class TCPClient {
     
     public var connection: NWConnection?
     public var latestReceivedMessage: String = ""
     public var log: [String] = []
+    public var host: NWEndpoint.Host
+    public var port: NWEndpoint.Port
+    
     
     init() {
-        self.connection = NWConnection(host: Settings.host, port: Settings.port, using: .tcp)
+        //Initializes the connection with the given host and port. Connection is not running yet
+        self.connection = NWConnection(host: host, port: port, using: .tcp)
+    }
+    
+    init(host: NWEndpoint.Host?, port: NWEndpoint.Port?) {
+        if let host = host{
+            self.host = host
+        }
+        
+        if let port = port{
+            self.port = port
+        }
+        
+        self.connection = NWConnection(host: host, port: port, using: .tcp)
     }
     
     public func startConnection(value: Data){
@@ -26,20 +42,23 @@ public final class FinalClient {
             self.log.append("Client will start connection...Current state: \(state)")
             switch state{
             case .setup:
-
                 self.log.append("Setup")
+                self.setUpConnection()
                 break
             case .waiting(_):
                 self.log.append("Waiting")
-                self.connectionFailed()
+                self.connectionWaiting()
+//                self.connectionFailed()
             case .preparing:
                 self.log.append("Preparing")
+                self.connectionPreparing()
                 break
             case .ready:
                 self.log.append("Ready")
             case .failed(_):
-                self.log.append("Connection failed")
-                self.connection?.cancel()
+//                self.log.append("Connection failed")
+                self.connectionFailed
+//                self.connection?.cancel()
             case .cancelled:
                 self.log.append("Connection cancelled")
                 break
@@ -54,9 +73,9 @@ public final class FinalClient {
         self.connection?.start(queue: .main)
     }
     
-    public func receiveMessage(){
+    public func receiveMessage(minLength min: Int = 1, maxLength max: Int){
         
-        self.connection?.receive(minimumIncompleteLength: 1, maximumLength: 65536, completion: { data, _, isComplete, error in
+        self.connection?.receive(minimumIncompleteLength: min, maximumLength: max, completion: { data, _, isComplete, error in
             if let data = data, !data.isEmpty{
                 
                 self.log.append("---------------------------\nClient sent data: \n\(String(data: data, encoding: .utf8) ?? "error")")
@@ -98,6 +117,28 @@ public final class FinalClient {
         self.connection?.cancel()
     }
     
+    //MARK: - Maybe these should be computed variables that only have 'set'
+    
+    //Determines what should be executed during the setup of the connection
+    public func setUpConnection(_ setup: (() -> Void)?){
+        if let setup = setup{
+            setup()
+        }
+    }
+    
+    //For the waiting stage. So far it seems that its best to cancel the connection here. The "connectionFailed" function was being used in
+    //the waiting stage originally
+    public func connectionWaiting(_ waiting: (() -> Void)?){
+        if let waiting = waiting{
+            waiting()
+        }
+    }
+    
+    public func connectionPreparing(_ preparing: (() -> Void)?){
+        if let preparing = preparing{
+            preparing()
+        }
+    }
     
 }
 

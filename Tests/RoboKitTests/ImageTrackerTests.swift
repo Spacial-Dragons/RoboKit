21678ab0a367c541757c4f094dcf64460e689055
Matvei Novoselov <matveynovoselov@gmail.com>
2025-04-24 18:55:20 +0200

    Added compute root position test

--- Tests/RoboKitTests/ImageTrackerTests.swift
+++ Tests/RoboKitTests/ImageTrackerTests.swift
@@ -11,12 +11,14 @@ struct ImageTrackerTests {
     private var tracker: ImageTracker
     
     init() {
+        // Initialize tracking images defined in Test Assets.xcassets
         self.trackingImagesConfig = [
             TrackingImage(imageName: "img_1", rootOffset: .init(x: 1.1, y: 2.2, z: 3.0)),
             TrackingImage(imageName: "img_2", rootOffset: .init(x: -1.0, y: -3.5, z: 0.0)),
             TrackingImage(imageName: "img_3", rootOffset: .init(x: 10.2, y: -10.2, z: -3.0)),
         ]
         
+        // Initialize ImageTracker with ARResourceGroup from Test Assets.xcassets
         self.tracker = .init(
             arResourceGroupName: "RoboKitTestsArResourceGroup",
             images: trackingImagesConfig
@@ -27,15 +29,18 @@ struct ImageTrackerTests {
     func getTrackedImagesTransformTest() {
         // Test that getTrackedImagesTransform() returns the expected default transforms in simulator.
         let trackedImagesTransforms: [simd_float4x4] = tracker.getTrackedImagesTransform()
+        #expect(trackedImagesTransforms.count == trackingImagesConfig.count)
+        
         for i in 0..<trackedImagesTransforms.count {
             #expect(trackedImagesTransforms[i].position == trackingImagesConfig[i].rootOffset)
         }
     }
     
-    @Test("Compute root position")
-    func computeRootPositionTest() {
+    @Test("Compute root position in simulator")
+    func computeRootPositionSimulatorTest() {
         // Test the averaging logic in computeRootPosition() for correctly computing the root transform based on multiple anchors.
-        #expect(true)
+        let rootPosition = tracker.computeRootPosition()
+        #expect(rootPosition?.position == .zero)
     }
     
 }

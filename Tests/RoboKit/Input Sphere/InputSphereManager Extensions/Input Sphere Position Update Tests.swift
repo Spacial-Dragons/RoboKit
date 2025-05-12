b296980f4fedb485df47b685d4fcf3862c34eecc
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2025-05-12 16:24:29 +0200

    Add Input Sphere Position Update test

---
+++ Tests/RoboKit/Input Sphere/InputSphereManager Extensions/Input Sphere Position Update Tests.swift
@@ -0,0 +1,31 @@
+//
+//  Input Sphere Position Update Tests.swift
+//  RoboKit
+//
+//  Created by Mariia Chemerys on 12.05.2025.
+//
+
+import Testing
+import RealityKit
+@testable import RoboKit
+
+@Suite("InputSphereManager Extensions - Input Sphere Update Methods")
+@MainActor
+struct UpdateInputSpherePositionTests {
+
+    var manager = InputSphereManager()
+    var root = Entity()
+    var sphere = Entity()
+
+    @Test("Input Sphere position updates correctly")
+    func testPositionUpdated() {
+        sphere.position = [1, 2, 3]
+        root.position = [0, 0, 1]
+
+        manager.inputSphere = sphere
+        manager.updateInputSpherePosition(rootPoint: root)
+
+        #expect(manager.inputSpherePositionRelativeToParent == SIMD3<Float>(1, 2, 3))
+        #expect(manager.inputSpherePositionRelativeToRoot == sphere.position(relativeTo: root))
+    }
+}

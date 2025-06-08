c9a629a9574916a5794809510312061110a651ad
Matvei Novoselov <matveynovoselov@gmail.com>
2025-06-08 23:52:16 +0200

    Updated Unit Tests to match changes from 06.06.2025

--- Tests/RoboKit/Input Sphere/InputSphereManager/Input Sphere Update Methods/InputSphereRotationUpdateTests.swift
+++ Tests/RoboKit/Input Sphere/InputSphereManager/Input Sphere Update Methods/InputSphereRotationUpdateTests.swift
@@ -31,14 +31,14 @@ struct UpdateInputSphereRotationTests {
         let testCases: [(EulerAngles: [EulerAngle: Float], expectedRotation: simd_quatf)] = [
             (
                 [.roll: .pi / 4, .yaw: .pi / 2, .pitch: .pi / 3],
-                simd_quatf(angle: .pi / 4, axis: [0, 0, 1]) *
-                simd_quatf(angle: .pi / 2, axis: [0, 1, 0]) *
+                simd_quatf(angle: .pi / 4, axis: [0, 1, 0]) *
+                simd_quatf(angle: .pi / 2, axis: [0, 0, 1]) *
                 simd_quatf(angle: .pi / 3, axis: [1, 0, 0])
             ),
             (
                 [.roll: .pi / 2, .yaw: .pi / 4, .pitch: .pi / 3],
-                simd_quatf(angle: .pi / 2, axis: [0, 0, 1]) *
-                simd_quatf(angle: .pi / 4, axis: [0, 1, 0]) *
+                simd_quatf(angle: .pi / 2, axis: [0, 1, 0]) *
+                simd_quatf(angle: .pi / 4, axis: [0, 0, 1]) *
                 simd_quatf(angle: .pi / 3, axis: [1, 0, 0])
             )
         ]

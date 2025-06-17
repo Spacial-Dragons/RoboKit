2a9dc91ec62d5b209afd4934fe1920403070a6f8
Sofia Diniz Melo Santos <sdms@cin.ufpe.br>
2025-06-17 16:38:46 +0200

    renamed input sphere to input entity on documentation files

--- Tests/RoboKit/Input Sphere/Enums/InputSphereAxisTests.swift
+++ Tests/RoboKit/Input Sphere/Enums/InputSphereAxisTests.swift
@@ -18,15 +18,15 @@ import RealityKit
 import simd
 @testable import RoboKit
 
-@Suite("InputSphereAxis")
-struct InputSphereAxisTests {
+@Suite("InputEntityAxis")
+struct InputEntityAxisTests {
 
     @Test("Orientation values match expected quaternions")
     func testOrientationValues() {
         let tolerance: Float = 1e-5
 
         // Lateral: rotation -π/2 around Z
-        if let lateralOrientation = InputSphereAxis.lateral.orientation {
+        if let lateralOrientation = InputEntityAxis.lateral.orientation {
             let expected = simd_quatf(angle: -.pi / 2, axis: [0, 0, 1])
             #expect(abs(lateralOrientation.angle - expected.angle) < tolerance)
             #expect(dot(lateralOrientation.axis, expected.axis) > 0.9999)
@@ -35,10 +35,10 @@ struct InputSphereAxisTests {
         }
 
         // Vertical: nil
-        #expect(InputSphereAxis.vertical.orientation == nil)
+        #expect(InputEntityAxis.vertical.orientation == nil)
 
         // Longitudinal: rotation -π/2 around X
-        if let longitudinalOrientation = InputSphereAxis.longitudinal.orientation {
+        if let longitudinalOrientation = InputEntityAxis.longitudinal.orientation {
             let expected = simd_quatf(angle: -.pi / 2, axis: [1, 0, 0])
             #expect(abs(longitudinalOrientation.angle - expected.angle) < tolerance)
             #expect(dot(longitudinalOrientation.axis, expected.axis) > 0.9999)
